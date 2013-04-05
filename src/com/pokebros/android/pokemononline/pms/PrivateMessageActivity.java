7a46e4784be30f9be2877d15bc47c89e1c3a5cde
coyotte508 <coyotte508@hotmail.com>
2013-04-05 03:20:57 +0200

    more PM work
    Now if you have the pm activity open, you should be able to see incoming pms

--- src/com/pokebros/android/pokemononline/pms/PrivateMessageActivity.java
+++ src/com/pokebros/android/pokemononline/pms/PrivateMessageActivity.java
@@ -10,6 +10,7 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
@@ -18,8 +19,9 @@ import com.pokebros.android.pokemononline.NetworkService;
 import com.pokebros.android.pokemononline.R;
 import com.pokebros.android.pokemononline.pms.PrivateMessageList.PrivateMessageListListener;
 
-public class PrivateMessageActivity extends Activity implements PrivateMessageListListener {
+public class PrivateMessageActivity extends Activity {
 	protected PrivateMessageList pms;
+	protected MyAdapter adapter = new MyAdapter();
 
 	/** Called when the activity is first created. */
     @Override
@@ -29,22 +31,29 @@ public class PrivateMessageActivity extends Activity implements PrivateMessageLi
         bindService(new Intent(this, NetworkService.class), connection,
         		Context.BIND_AUTO_CREATE);
         
-        setContentView(R.layout.pm_activity);        
+        setContentView(R.layout.pm_activity);
+        ViewPager vp = (ViewPager) findViewById(R.id.viewPager);
+        vp.setAdapter(adapter);
     }
     
     private ServiceConnection connection = new ServiceConnection() {
 		public void onServiceConnected(ComponentName className, IBinder service) {
 			NetworkService netServ = ((NetworkService.LocalBinder)service).getService();
 			pms = netServ.pms;
-			pms.listener = PrivateMessageActivity.this;
+			pms.listener = adapter;
+			adapter.notifyDataSetChanged();
 		}
 		
 		public void onServiceDisconnected(ComponentName className) {
 		}
 	};
 	
-	private class MyAdapter extends PagerAdapter
+	private class MyAdapter extends PagerAdapter implements PrivateMessageListListener
 	{
+		MyAdapter() {
+			pms.listener = this;
+		}
+		
 		@Override
 		public int getCount() {
 			return pms.privateMessages.size();
@@ -61,23 +70,24 @@ public class PrivateMessageActivity extends Activity implements PrivateMessageLi
 			//TODO: recycle views?
 			ListView lv = new ListView(PrivateMessageActivity.this);
 			container.addView(lv);
+			
+			lv.setAdapter(new PrivateMessageAdapter(PrivateMessageActivity.this, pm));
 
-			return null;
+			return lv;
 		}
 
 		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {
-			return (Object)arg0 == arg1;
+		public boolean isViewFromObject(View v, Object o) {
+			return (Object)v == o;
 		}
 
 		@Override
 		public void destroyItem(ViewGroup container, int position, Object object) {
 			container.removeView((View)object);
 		}
-	}
 
-	public void onNewPM(PrivateMessage privateMessage) {
-		//Todo: manage adapter & all
+		public void onNewPM(PrivateMessage privateMessage) {
+			notifyDataSetChanged();
+		}
 	}
-
 }
