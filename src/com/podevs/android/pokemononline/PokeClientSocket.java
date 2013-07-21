39d078c440abfb1eab77652c1ec23cf49d7ae868
Mike <mikewando@gmail.com>
2013-07-21 18:32:23 -0400

    Fix indentation? I've never had much luck with Eclipse and indentation.

--- src/com/podevs/android/pokemononline/PokeClientSocket.java
+++ src/com/podevs/android/pokemononline/PokeClientSocket.java
@@ -12,42 +12,42 @@ import com.podevs.android.utilities.Bais;
 import com.podevs.android.utilities.Baos;
 
 public class PokeClientSocket {
-	// Socket channel, true connection.
-	private SocketChannel schan = null;
-	@SuppressWarnings("unused")
-	private final static String TAG = "PokeClientSocket";
-	// 4 bytes for length and 1 for type.
+    // Socket channel, true connection.
+    private SocketChannel schan = null;
+    @SuppressWarnings("unused")
+    private final static String TAG = "PokeClientSocket";
+    // 4 bytes for length and 1 for type.
     private final static int OVERHEAD_SIZE = 5;
-	public final static int CONNECT_TIMEOUT = 10000;
+    public final static int CONNECT_TIMEOUT = 10000;
 
-	public PokeClientSocket(String inIpAddr, int inPortNum) throws SocketTimeoutException, IOException
-	{
-		schan = SocketChannel.open();
-		schan.socket().connect(new InetSocketAddress(inIpAddr, inPortNum), CONNECT_TIMEOUT);
-	}
+    public PokeClientSocket(String inIpAddr, int inPortNum) throws SocketTimeoutException, IOException
+    {
+        schan = SocketChannel.open();
+        schan.socket().connect(new InetSocketAddress(inIpAddr, inPortNum), CONNECT_TIMEOUT);
+    }
 
-	public boolean isConnected() {
-		boolean ret = false;
-		try {
-			ret = schan.finishConnect() && schan.isConnected();
-		} catch (IOException e) {
-			return false;
-		}
-		return ret;
-	}
+    public boolean isConnected() {
+        boolean ret = false;
+        try {
+            ret = schan.finishConnect() && schan.isConnected();
+        } catch (IOException e) {
+            return false;
+        }
+        return ret;
+    }
 
-	class NetSender implements Runnable {
-		Baos msgToSend;
-		byte msgType;
-		
-		public NetSender(Baos msgToSend, int msgType) {
-			this.msgToSend = msgToSend;
-			this.msgType = (byte)msgType;
-		}
-		
-		public void run() {
-			try {
-			    ByteBuffer buf;
+    class NetSender implements Runnable {
+        Baos msgToSend;
+        byte msgType;
+
+        public NetSender(Baos msgToSend, int msgType) {
+            this.msgToSend = msgToSend;
+            this.msgType = (byte)msgType;
+        }
+
+        public void run() {
+            try {
+                ByteBuffer buf;
 
                 if (msgToSend == null) {
                     // "Empty" message
@@ -55,34 +55,34 @@ public class PokeClientSocket {
                     buf.order(ByteOrder.BIG_ENDIAN);
                     buf.putInt(1);
                     buf.put(msgType);
-		        } else {
-	                buf = ByteBuffer.allocate(OVERHEAD_SIZE + msgToSend.size());
-	                buf.order(ByteOrder.BIG_ENDIAN);
-		            buf.putInt(1 + msgToSend.size());
-		            buf.put(msgType);
-		            buf.put(msgToSend.toByteArray());
-		        }
-		        
-				buf.rewind();
-				schan.write(buf);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public void sendMessage(Baos msgToSend, Command msgType) {
-		new Thread(new NetSender(msgToSend, msgType.ordinal())).start();
-	}
+                } else {
+                    buf = ByteBuffer.allocate(OVERHEAD_SIZE + msgToSend.size());
+                    buf.order(ByteOrder.BIG_ENDIAN);
+                    buf.putInt(1 + msgToSend.size());
+                    buf.put(msgType);
+                    buf.put(msgToSend.toByteArray());
+                }
 
-	/**
-	 * Reads exactly one message from the socket and returns it as a Bais.
-	 * 
-	 * @throws IOException forwarded from the socket functions
-	 * @throws ParseException if the server sent a packet too big for us to handle
-	 */
-	public Bais getMsg() throws IOException, ParseException {
-	    ByteBuffer packetLength = ByteBuffer.allocate(4);
+                buf.rewind();
+                schan.write(buf);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessage(Baos msgToSend, Command msgType) {
+        new Thread(new NetSender(msgToSend, msgType.ordinal())).start();
+    }
+
+    /**
+     * Reads exactly one message from the socket and returns it as a Bais.
+     * 
+     * @throws IOException forwarded from the socket functions
+     * @throws ParseException if the server sent a packet too big for us to handle
+     */
+    public Bais getMsg() throws IOException, ParseException {
+        ByteBuffer packetLength = ByteBuffer.allocate(4);
         ByteBuffer data = ByteBuffer.allocate(4096);
 
         while (packetLength.position() < packetLength.capacity()) {
@@ -97,7 +97,7 @@ public class PokeClientSocket {
 
         Baos msg = new Baos();
         while (remaining > 0) {
-		    // Don't read more than this message.
+            // Don't read more than this message.
             data.clear();
             data.limit(Math.min(remaining, data.capacity()));
             schan.read(data);
@@ -106,17 +106,17 @@ public class PokeClientSocket {
             data.flip();
             msg.write(data.array(), 0, data.limit());
         }
-        
+
         Bais ret = new Bais(msg.toByteArray());
         msg.close();
         return ret;
-	}
+    }
 
-	public void close() {
-		try {
-			schan.close();
-		} catch (IOException e) {
-			// TODO Should this be thrown or caught?
-		}
-	}
+    public void close() {
+        try {
+            schan.close();
+        } catch (IOException e) {
+            // TODO Should this be thrown or caught?
+        }
+    }
 }
