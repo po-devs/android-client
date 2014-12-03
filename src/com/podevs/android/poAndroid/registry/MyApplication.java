206ea12c0549c8f45bf2d9b68e0b191fcfc5b0ae
Jonathan Miller <jonjmiller4991@gmail.com>
2014-12-02 22:58:13 -0500

    Google Activity

---
+++ src/com/podevs/android/poAndroid/registry/MyApplication.java
@@ -0,0 +1,31 @@
+package com.podevs.android.poAndroid.registry;
+
+import android.app.Application;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+import com.podevs.android.poAndroid.R;
+
+import java.util.HashMap;
+
+public class MyApplication extends Application {
+    private static final String PROPERTY_ID = "";
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+        GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg: roll-up tracking.
+        ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from a company.
+    }
+
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+    synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker(PROPERTY_ID)
+                    : (trackerId == TrackerName.GLOBAL_TRACKER) ? analytics.newTracker(R.xml.global_tracker)
+                    : analytics.newTracker(R.xml.global_tracker);
+            mTrackers.put(trackerId, t);
+
+        }
+        return mTrackers.get(trackerId);
+    }
+}
