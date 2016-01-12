46dee0c4b7a334b410c0d14f7c0a7d76c5722f84
Berkan Diler <thetoerk@gmail.com>
2016-01-12 21:48:07 +0100

    Fix "...was dragged out" message (not tested)
    Can someone maybe confirm that this is working?
    I can test it if necessary, but it could take some days because I have to finish school staff and I don't have installed IntelliJ on this computer, yet.

--- src/com/podevs/android/poAndroid/battle/SpectatingBattle.java
+++ src/com/podevs/android/poAndroid/battle/SpectatingBattle.java
@@ -509,6 +509,7 @@ public class SpectatingBattle {
 				s = s.replaceAll("%tf", players[(player == 0 ? 1 : 0)].nick());
 				if(type  != -1) s = s.replaceAll("%t", Type.values()[type].toString());
 				if(foe   != -1) s = s.replaceAll("%f", pokes[foe][slot].nick);
+				if(foe   != -1) s = s.replaceAll("%e", pokes[foe][slot].rnick);
 				if(other  != -1 && s.contains("%m")) s = s.replaceAll("%m", MoveInfo.name(other));
 				s = s.replaceAll("%d", new Short(other).toString());
 				s = s.replaceAll("%q", q);
