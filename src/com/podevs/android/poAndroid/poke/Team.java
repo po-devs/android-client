a91d8ef8de94dcab972cd6f630fa22dd7cc4d7d9
Berkan Diler <b.diler@gmx.de>
2017-01-04 15:14:16 +0100

    Fix crash when saving teams on Android 7.0

--- src/com/podevs/android/poAndroid/poke/Team.java
+++ src/com/podevs/android/poAndroid/poke/Team.java
@@ -133,7 +133,7 @@ public class Team implements SerializeBytes {
   //      StringWriter writer = new StringWriter();
    //     StreamResult result1 = new StreamResult(writer);
 		try {
-			result = new StreamResult(c.openFileOutput(c.getSharedPreferences("team", 0).getString("file", "team.xml"), Context.MODE_WORLD_READABLE));
+			result = new StreamResult(c.openFileOutput(c.getSharedPreferences("team", 0).getString("file", "team.xml"), Context.MODE_PRIVATE));
         } catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 			return;
