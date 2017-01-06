db2302a217eb6ba243d52dbc7f6d0fdfd4c80645
MidwayMarshall <jonjmiller4991@gmail.com>
2017-01-06 17:38:01 -0500

    Remove Reflect usage

---
+++ src/com/podevs/android/utilities/MyColor.java
@@ -0,0 +1,116 @@
+package com.podevs.android.utilities;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+public class MyColor {
+    public static final int BLACK       = 0xFF000000;
+
+    public static int parseColor(String colorString) {
+        if (colorString.charAt(0) == '#') {
+            // Use a long to avoid rollovers on #ffXXXXXX
+            long color = Long.parseLong(colorString.substring(1), 16);
+            if (colorString.length() == 7) {
+                // Set the alpha value
+                color |= 0x00000000ff000000;
+            } else if (colorString.length() != 9) {
+                throw new IllegalArgumentException("Unknown color");
+            }
+            return (int)color;
+        } else {
+            Integer color = sColorNameMap.get(colorString.toLowerCase(Locale.ROOT));
+            if (color != null) {
+                return color;
+            }
+        }
+        throw new IllegalArgumentException("Unknown color");
+    }
+
+    public static int getHtmlColor(String color) {
+        Integer i = sColorNameMap.get(color.toLowerCase(Locale.ROOT));
+        if (i != null) {
+            return i;
+        } else {
+            try {
+                return convertValueToInt(color, -1);
+            } catch (NumberFormatException nfe) {
+                return -1;
+            }
+        }
+    }
+
+    public static final int convertValueToInt(CharSequence charSeq, int defaultValue) {
+        if (null == charSeq)
+            return defaultValue;
+
+        String nm = charSeq.toString();
+
+        // XXX This code is copied from Integer.decode() so we don't
+        // have to instantiate an Integer!
+
+        int value;
+        int sign = 1;
+        int index = 0;
+        int len = nm.length();
+        int base = 10;
+
+        if ('-' == nm.charAt(0)) {
+            sign = -1;
+            index++;
+        }
+
+        if ('0' == nm.charAt(index)) {
+            //  Quick check for a zero by itself
+            if (index == (len - 1))
+                return 0;
+
+            char    c = nm.charAt(index + 1);
+
+            if ('x' == c || 'X' == c) {
+                index += 2;
+                base = 16;
+            } else {
+                index++;
+                base = 8;
+            }
+        }
+        else if ('#' == nm.charAt(index))
+        {
+            index++;
+            base = 16;
+        }
+
+        return Integer.parseInt(nm.substring(index), base) * sign;
+    }
+
+    private static final HashMap<String, Integer> sColorNameMap;
+
+    static {
+        sColorNameMap = new HashMap<String, Integer>();
+        sColorNameMap.put("black", 0xFF000000);
+        sColorNameMap.put("darkgray", 0xFF444444);
+        sColorNameMap.put("gray", 0xFF888888);
+        sColorNameMap.put("lightgray", 0xFFCCCCCC);
+        sColorNameMap.put("white", 0xFFFFFFFF);
+        sColorNameMap.put("red", 0xFFFF0000);
+        sColorNameMap.put("green", 0xFF00FF00);
+        sColorNameMap.put("blue", 0xFF0000FF);
+        sColorNameMap.put("yellow", 0xFFFFFF00);
+        sColorNameMap.put("cyan", 0xFF00FFFF);
+        sColorNameMap.put("magenta", 0xFFFF00FF);
+        sColorNameMap.put("aqua", 0xFF00FFFF);
+        sColorNameMap.put("fuchsia", 0xFFFF00FF);
+        sColorNameMap.put("darkgrey", 0xFF444444);
+        sColorNameMap.put("grey", 0xFF888888);
+        sColorNameMap.put("lightgrey", 0xFFCCCCCC);
+        sColorNameMap.put("lime", 0xFF00FF00);
+        sColorNameMap.put("maroon", 0xFF800000);
+        sColorNameMap.put("navy", 0xFF000080);
+        sColorNameMap.put("olive", 0xFF808000);
+        sColorNameMap.put("purple", 0xFF800080);
+        sColorNameMap.put("silver", 0xFFC0C0C0);
+        sColorNameMap.put("teal", 0xFF008080);
+        sColorNameMap.put("orange", 0xFFFFA500);
+        sColorNameMap.put("pink", 0xFFFFC0CB);
+    }
+}
