ad53d822d6ab20875368d659d50c45db4d206bd6
MidwayMarshall <MidwayMarshall@users.noreply.github.com>
2017-01-13 11:52:17 -0500

    Fix ev manual input (#260)

--- src/com/podevs/android/poAndroid/teambuilder/EVSlider.java
+++ src/com/podevs/android/poAndroid/teambuilder/EVSlider.java
@@ -1,6 +1,8 @@
 package com.podevs.android.poAndroid.teambuilder;
 
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -25,6 +27,7 @@ public class EVSlider {
 		slider = (SeekBar)lay.findViewById(R.id.slider);
 		label = (TextView)lay.findViewById(R.id.label);
 		edit = (EditText)lay.findViewById(R.id.edit);
+		edit.setImeOptions(EditorInfo.IME_ACTION_DONE);
 		label.setText(StatsInfo.Shortcut(stat));
 		
 		slider.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
@@ -49,6 +52,19 @@ public class EVSlider {
 				if (listener != null) {
 					listener.onEVChanged(stat, pr);
 				}
+			}
+		});
+
+		edit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_DONE) {
+					if (listener != null) {
+						listener.onEVChanged(stat, Integer.parseInt(edit.getText().toString()));
+					}
+					return true;
+				}
+				return false;
 			}
 		});
 	}
